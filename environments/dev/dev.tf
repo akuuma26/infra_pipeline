23f26ca963045360d41558e85fc4dd6b2c691e27
akuuma26 <diwanawelfare01@gmail.com>
2026-08-22 20:20:08 +0530

    added

--- environments/dev/dev.tf
+++ environments/dev/dev.tf
@@ -1,10 +1,10 @@
 module "resource_group" {
-  source   = "../../modules/resource_group"
- rg = var.rg
+  source = "../../modules/resource_group"
+  rg     = var.rg
 }
 
 module "azure vnet" {
   depends_on = [module.resource_group]
-  source   = "../../modules/resource_group"
- vnet = var.vnet
+  source     = "../../modules/resource_group"
+  vnet       = var.vnet
 }
