b8b1e06237e9d23a2b1e5439dedb5643205db9e5
akuuma26 <diwanawelfare01@gmail.com>
2026-08-23 18:38:59 +0530

    feat: add backend

---
+++ environments/dev/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "tfstateabhi2026"
+    container_name       = "tfstate"
+    key                  = "dev.tfstate"
+    use_azuread_auth     = true
+  }
+}
