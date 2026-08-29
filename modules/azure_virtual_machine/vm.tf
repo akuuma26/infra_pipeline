a198743e8727df34af8bc3ba0b4bf894dfb9aa97
akuuma26 <diwanawelfare01@gmail.com>
2026-08-29 18:24:41 +0530

    feat: added VM modules

--- modules/azure_virtual_machine/vm.tf
+++ modules/azure_virtual_machine/vm.tf
@@ -14,4 +14,11 @@ resource "azurerm_linux_virtual_machine" "linuxVm" {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
+
+  source_image_reference {
+  publisher = "Canonical"
+  offer     = "0001-com-ubuntu-server-jammy"
+  sku       = "22_04-lts"
+  version   = "latest"
+}
 }
