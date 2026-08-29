f3158a48a9c03daa28d06e22c4f1546a3cb5e48d
akuuma26 <diwanawelfare01@gmail.com>
2026-08-29 18:16:37 +0530

    feat: added VM modules

--- environments/dev/dev.tf
+++ environments/dev/dev.tf
@@ -1,34 +1,34 @@
 module "resourceGroup" {
-  source = "../childModule/resourceGroup/"
+  source = "../../modules/resource_group/"
   rg     = var.rg
 }
 
 module "virtual_network" {
   depends_on = [module.resourceGroup]
-  source     = "../childModule/virtualnetwork"
+  source     = "../../modules/Virtual_network"
   vnet       = var.vnet
 }
 
 module "subnet" {
   depends_on = [module.virtual_network]
-  source     = "../childModule/subnet"
+  source     = "../../modules/subnet"
   subnet     = var.subnet
 }
 
 module "public_ip" {
   depends_on = [module.resourceGroup]
-  source     = "../childModule/azure_pip"
+  source     = "../../modules/azure_pip"
   pip        = var.pip
 }
 
 module "network_interface" {
   depends_on = [module.public_ip]
-  source     = "../childModule/azure_nic"
+  source     = "../../modules/azure_nic"
   nic        = var.nic
 }
 
 module "linux_vm" {
   depends_on = [module.network_interface]
-  source     = "../childModule/linux_virtual_machine"
-  linuxVm    = var.linuxVm
+  source     = "../../modules/azure_virtual_machine"
+  linuxVm = var.linuxVm
 }
